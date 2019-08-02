1b5ceb0a61f76204ec07f5b02187df4a3e8f2961
matteo <matteo.devita2000@gmail.com>
2019-08-02 19:51:44 +0200

    index.js ### index.js * Update to match heroku rules

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ client.on('message', message => {
 		const mentioned_user = message.mentions.members.first();
 		if (!mentioned_user) {
 			message.reply('You have to mention a user you dummy');
-			return FAILURE;
+			return process.env.FAILURE;
 		}
 		if (!mentioned_user.kickable) {
 			message.reply(`I don't have the permissions to kick this user`);
-			return FAILURE;
+			return process.env.FAILURE;
 		}
 		message.reply(`Do you really want to kick ${mentioned_user.user.tag} ? (Answer with "YES" or "NO")`);
 		const collector = message.channel.createCollector(
@@ -45,19 +45,19 @@ client.on('message', message => {
 				.then(() => message.reply(`${mentioned_user.user.tag} has been kicked. ciao !`))
 				.catch(error => message.reply(`I couldn't kick this user, sorry. (error code : ${error})`))
 				collector.stop()
-				return SUCCESS;
+				return process.env.SUCCESS;
 			}
 			else if (collector_answer.content === 'NO') {
 				message.reply('OK, mission aborted !');
 				collector.stop('No kick');
-				return FAILURE;
+				return process.env.FAILURE;
 			}
 			else {
 				message.reply('This is not a good answer, please answer with "YES" or "NO"');
 			}
 		})
-		return FAILURE;
+		return process.env.FAILURE;
 	}
 })
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
