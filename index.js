6117d5d5e87bef6b8b4452d99523df3632fdfa35
matteo <matteo.devita2000@gmail.com>
2019-08-07 13:39:55 +0200

    Update help command ### index.js * The bot now shows a message with all the commands read from commands.JSON file

--- index.js
+++ index.js
@@ -64,13 +64,54 @@ client.on('message', message => {
 		fs.readFile('./ressources/commands.JSON', (err, data) => {
 			const commands = JSON.parse(data.toString())
 			message.reply(` the commands are :\n${commands}`)
-			// console.log(commands.administration);
-			// console.log(`${JSON.stringify(commands)}`)
+			const categories = Object.keys(commands);
+			for (let currentCategorie of categories) {
+				message.channel.send(`${currentCategorie}`)
+				console.log(currentCategorie)
+				const currentCommands = Object.keys(commands[currentCategorie])
+				for (let currentCommand of currentCommands) {
+					console.log(`	${currentCommand}: ${commands[currentCategorie][currentCommand]}`)
+					message.channel.send(`\t\`${currentCommand}\`: ${commands[currentCategorie][currentCommand]}`)
+				}
+					//console.log(currentCommand)
+					//console.log(commands[currentCategorie][currentCommand])
+			}
+			// console.log(`categories = ${categories}`);
+			// console.log(`${JSON.stringify(commands.administration)}`)
 			// for (let i in commands) {			
 			// 		message.reply(`${JSON.stringify(i)}`)				
 			// 	//console.log(commands[i])
 			// 	//message.reply(`${i}`)
 			// }
+			// message.channel.send({embed: {
+			// 	color: 3447003,
+			// 	author: {
+			// 	  name: client.user.username,
+			// 	  icon_url: client.user.avatarURL
+			// 	},
+			// 	title: "This is an embed",
+			// 	url: "http://google.com",
+			// 	description: "This is a test embed\nto showcase what they look like and what they can do.",
+			// 	fields: [{
+			// 		name: "Fields",
+			// 		value: "They can have different fields with small headlines."
+			// 	  },
+			// 	  {
+			// 		name: "Masked links",
+			// 		value: "You can put [masked links](http://google.com) inside of rich embeds."
+			// 	  },
+			// 	  {
+			// 		name: "Markdown",
+			// 		value: "You can put all the *usual* **__Markdown__** inside of them."
+			// 	  }
+			// 	],
+			// 	timestamp: new Date(),
+			// 	footer: {
+			// 	  icon_url: client.user.avatarURL,
+			// 	  text: "© Example"
+			// 	}
+			//   }
+			// });
 		})
 	}
 })
